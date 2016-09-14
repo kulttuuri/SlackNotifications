af098098bc6cb405ef5cf60a86fdbae21c2eded0
Meneth <github@meneth.com>
2016-09-14 20:30:47 +0200

    Pointless comment removed

--- SlackNotifications/SlackNotifications.php
+++ SlackNotifications/SlackNotifications.php
@@ -1,7 +1,6 @@
 <?php
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'SlackNotifications' );
-	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	wfWarn(
 		'Deprecated PHP entry point used for FooBar extension. ' .
 		'Please use wfLoadExtension instead, ' .
