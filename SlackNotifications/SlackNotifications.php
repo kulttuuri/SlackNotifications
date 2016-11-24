dbe69ebd613962feac29be1f1bb04c88b6866f76
Aleksi Postari <aleksi@teachergaming.com>
2016-11-24 15:48:16 +0200

    Foobar -> SlackNotifications

--- SlackNotifications/SlackNotifications.php
+++ SlackNotifications/SlackNotifications.php
@@ -2,11 +2,11 @@
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'SlackNotifications' );
 	wfWarn(
-		'Deprecated PHP entry point used for FooBar extension. ' .
+		'Deprecated PHP entry point used for SlackNotifications extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
 	);
 	return;
 } else {
-	die( 'This version of the FooBar extension requires MediaWiki 1.25+' );
+	die( 'This version of the SlackNotifications extension requires MediaWiki 1.25+' );
 }
